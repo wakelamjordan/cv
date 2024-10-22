129d22d783f20e11b876294c7704dec2883c1fcd
jordan wakelam <jwakelams@gmail.com>
2024-10-23 01:35:18 +0200

    pas finit le print mais je vais quand même mettre en ligne

--- cv/src/app/layout.js
+++ cv/src/app/layout.js
@@ -20,6 +20,25 @@ export const metadata = {
   title: "Curriculum Vitae de Jordan Wakelam - Développeur Web",
   description:
     "Découvrez le curriculum vitae de Jordan Wakelam, développeur web spécialisé en backend avec des compétences en Symfony, React et plus encore. Passionné, déterminé, et toujours en quête de nouvelles connaissances, il met son expertise et son adaptabilité au service de chaque projet.",
+  icons: {
+    icon: "/favicon/favicon-32x32.png",
+    shortcut: "/favicon/favicon-16x16.png",
+    apple: "/favicon/apple-touch-icon.png",
+    other: [
+      {
+        rel: "manifest",
+        url: "/site.webmanifest",
+      },
+      {
+        rel: "mask-icon",
+        url: "/favicon/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
+  },
+  themeColor: "#ffffff",
+  applicationName: "Jordan Wakelam Cv",
+  msapplicationTileColor: "#da532c",
 };
 
 export default function RootLayout({ children }) {
